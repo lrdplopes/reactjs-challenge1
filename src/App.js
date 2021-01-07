1a37dad26ef15e7617b576dafc4789b76a70e31a
lrdplopes <lrdp.lopes@gmail.com>
2021-01-07 11:12:43 -0300

    tests passed.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [projRepository, setProjRepository] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then((res) => {
+      setProjRepository(res.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const res = await api.post("/repositories", {
+      title: "Umbriel",
+      url: "https://github.com/lrdplopes/reactjs-challenge1",
+      techs: ["Node.js", "React.js"],
+    });
+
+    setProjRepository([...projRepository, res.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setProjRepository(projRepository.filter((project) => project.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projRepository.map((project) => (
+          <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
